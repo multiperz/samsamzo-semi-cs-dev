9e1f07dd4d8eea5dd4eadc0ebbc15c2b2a2550ca
user <user@DESKTOP-4AJ1TNQ>
2024-04-11 19:55:08 +0900

    service commit

--- samsamzo-semi-cs-dev/src/main/java/hong/cs/service/CSService.java
+++ samsamzo-semi-cs-dev/src/main/java/hong/cs/service/CSService.java
@@ -1,4 +1,13 @@
 package hong.cs.service;
 
+import java.util.ArrayList;
+
+import hong.cs.dto.CSDTO;
+
 public interface CSService {
+	public ArrayList<CSDTO> csSelectAll();
+	public CSDTO csSelect(int cs_number);
+	public CSDTO csInsert(CSDTO csDTO);
+	public CSDTO csUpdate(CSDTO csDTO);
+	public CSDTO csDelete(int cs_number);
 }
