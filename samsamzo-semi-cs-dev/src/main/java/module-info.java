2d484c0b7f7090d9aa97b3f52f05e08b6849fb32
user <user@DESKTOP-4AJ1TNQ>
2024-04-11 12:00:37 +0900

    package commit

---
+++ samsamzo-semi-cs-dev/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module hong.cs {
+	requires commons.dbcp2;
+	requires commons.logging;
+	requires org.apache.commons.pool2;
+	requires java.servlet;
+	requires java.servlet.jsp;
+	requires taglibs.standard.compat;
+	requires taglibs.standard.impl;
+	requires taglibs.standard.jstlel;
+	requires taglibs.standard.spec;
+	requires cos;
+}
